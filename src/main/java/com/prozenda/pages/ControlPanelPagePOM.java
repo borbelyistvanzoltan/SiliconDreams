280dfedb2ad4e43357efb673d0a2faca79c610e4
Balázs Paulcsik <balazs.paulcsik@prozenda.com>
2023-03-30 10:08:10 +0200

    Fixing

--- src/main/java/com/prozenda/pages/ControlPanelPagePOM.java
+++ src/main/java/com/prozenda/pages/ControlPanelPagePOM.java
@@ -25,6 +25,7 @@ public class ControlPanelPagePOM extends AbstractPage {
 
     public void clickNewProductButton() {
         getDriver().findElement(newProductButton).click();
+        getDriver().findElement(selectDataSheetConfirmationButton).click();
     }
 
     public void clickAllProductsButton() {
